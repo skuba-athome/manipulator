4ad513daefca35de70c24c94249036ea894518ce
fon <fon@localhost>
2016-02-27 01:02:17 +0700

    add script move arm right/left

---
+++ nodes/set_arm/right_dance_1.sh
@@ -0,0 +1,6 @@
+rostopic pub /dynamixel/right_shoulder_1_controller/command std_msgs/Float64 "data: -1.0" | 
+rostopic pub /dynamixel/right_shoulder_2_controller/command std_msgs/Float64 "data: 0.0" | 
+rostopic pub /dynamixel/right_elbow_controller/command std_msgs/Float64 "data: 0.3" | 
+rostopic pub /dynamixel/right_wrist_1_controller/command std_msgs/Float64 "data: 0.0" | 
+rostopic pub /dynamixel/right_wrist_2_controller/command std_msgs/Float64 "data: 1.5" | 
+rostopic pub /dynamixel/right_wrist_3_controller/command std_msgs/Float64 "data: 0.0"
