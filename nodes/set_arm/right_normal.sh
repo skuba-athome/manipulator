fcfc0a4b58a04166af66ea9a155f5135c5e15893
fon <fon@localhost>
2016-02-29 09:19:35 +0700

    update script normal right arm

--- nodes/set_arm/right_normal.sh
+++ nodes/set_arm/right_normal.sh
@@ -4,4 +4,4 @@ rostopic pub /dynamixel/right_elbow_controller/command std_msgs/Float64 "data: 0
 rostopic pub /dynamixel/right_wrist_1_controller/command std_msgs/Float64 "data: 0.0" | 
 rostopic pub /dynamixel/right_wrist_2_controller/command std_msgs/Float64 "data: 1.2" | 
 rostopic pub /dynamixel/right_wrist_3_controller/command std_msgs/Float64 "data: 0.0"|
-rostopic pub /dynamixel/right_gripper_joint_controller/command std_msgs/Float64 "data: -0.8"
+rostopic pub /dynamixel/right_gripper_joint_controller/command std_msgs/Float64 "data: 0.0"
